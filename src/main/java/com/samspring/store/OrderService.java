2ef2d7a81551a8926fc54fde3b4427d918b17129
Sammy D <mobilvodoo@googlemail.com>
2025-03-18 23:09:34 +0000

    Modify to decouple OrderService. Add PayPalPaymentService.

--- src/main/java/com/samspring/store/OrderService.java
+++ src/main/java/com/samspring/store/OrderService.java
@@ -1,8 +1,13 @@
 package com.samspring.store;
 
 public class OrderService {
+
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
     public void placeOrder() {
-        var paymentService = new StripePaymentService();
         paymentService.processPayment(10);
     }
 }
---
+++ src/main/java/com/samspring/store/PayPalPaymentService.java
@@ -0,0 +1,9 @@
+package com.samspring.store;
+
+public class PayPalPaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("PAYPAL");
+        System.out.println("Amount: " + amount);
+    }
+}
