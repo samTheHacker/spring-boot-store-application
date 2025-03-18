692bf9b428a7499ddacfb221d4a365bd59b8ddbf
Sammy D <mobilvodoo@googlemail.com>
2025-03-18 23:31:48 +0000

    Add @Autowire and @Service annotations. Add extra constructor to OrderService. Add setPaymentService method.

--- src/main/java/com/samspring/store/PayPalPaymentService.java
+++ src/main/java/com/samspring/store/PayPalPaymentService.java
@@ -1,5 +1,8 @@
 package com.samspring.store;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PayPalPaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
