031888d6197a240326545ebbe31edc2663d7bb19
Sammy D <mobilvodoo@googlemail.com>
2025-03-19 23:03:35 +0000

    Add primary and qualifier annotations to control bean selection

--- src/main/java/com/samspring/store/OrderService.java
+++ src/main/java/com/samspring/store/OrderService.java
@@ -1,5 +1,6 @@
 package com.samspring.store;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,7 +8,7 @@ public class OrderService {
 
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService){
+    public OrderService(@Qualifier("paypal") PaymentService paymentService){
         this.paymentService = paymentService;
     }
     public void placeOrder() {
--- src/main/java/com/samspring/store/StripePaymentService.java
+++ src/main/java/com/samspring/store/StripePaymentService.java
@@ -1,5 +1,10 @@
 package com.samspring.store;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
 public class StripePaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
